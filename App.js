66f857fea6704dcac16df5a2840a7783cfc122cc
Cole Anam <coleanam2000@gmail.com>
2023-10-26 23:51:59 -0400

    view patients and details page setup

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AddPatientsPage from './Components/AddPatientsPage';
 import ViewAllPatientsPage from './Components/ViewAllPatientsPage'
+import PatientDetails from './Components/PatientDetails'
 
 
 export default function App() {
   const Tab = createBottomTabNavigator();
+  const Stack = createNativeStackNavigator();
+
+  function PatientStack() {
+    return (
+      <Stack.Navigator >
+        <Stack.Screen name="Patients" component={ViewAllPatientsPage} />
+        <Stack.Screen name="PatientDetails" component={PatientDetails} />
+      </Stack.Navigator>
+    )
+  }
   
   return (
     <NavigationContainer>
