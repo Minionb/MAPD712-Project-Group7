dd9670396a6432645780b536f73ef853a6a08c8b
Cole Anam <coleanam2000@gmail.com>
2023-11-08 19:06:07 -0500

    Finished clinical data view page and delete use case

--- Components/PatientDetails.js
+++ Components/PatientDetails.js
@@ -1,9 +1,9 @@
 import { useEffect, useState, createContext, useContext } from "react";
-import { Text, StyleSheet, ScrollView, View, FlatList, TouchableOpacity } from 'react-native';
+import { Text, StyleSheet, ScrollView, View, FlatList, Alert } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { Feather, Octicons } from "@expo/vector-icons";
 import { MenuProvider } from 'react-native-popup-menu';
-import { Menu, MenuOptions, MenuOption, MenuTrigger, renderers } from 'react-native-popup-menu';
+import { Menu, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
 
 export const PageContext = createContext();
 
@@ -31,6 +31,7 @@ export default function PatientDetails({route, navigation}) {
 
     const Tab = createMaterialTopTabNavigator();
 
+    // Main details screen tab navigator
     return (
         <PageContext.Provider value={[patientDetails, setPatientDetails, testDataDetails, setTestDataDetails]}>
             <Tab.Navigator>
@@ -81,17 +82,20 @@ function ClinicalTestDataScreen() {
 
     const [testDataDetails, setTestDataDetails] = useState([])
     const [isLoading, setIsLoading] = useState(true);
+    const [refreshIndicator, setRefreshIndicator] = useState(false);
+
+    // Fetch test data for specific patient
+    const fetchTestDataDetails = async () => {
+        await fetch ('http://127.0.0.1:3000/patients/'+patientDetails._id+'/testdata')
+            .then(response => response.json())
+            .then(data => {
+                setTestDataDetails(data)
+                setIsLoading(false)
+            })
+            .catch(error => console.log(error))
+    };
 
     useEffect(() => {
-        const fetchTestDataDetails = async () => {
-            await fetch ('http://127.0.0.1:3000/patients/'+patientDetails._id+'/testdata')
-                .then(response => response.json())
-                .then(data => {
-                    setTestDataDetails(data)
-                    setIsLoading(false)
-                })
-                .catch(error => console.log(error))
-        };
         fetchTestDataDetails();
     }, []);
 
@@ -104,6 +108,7 @@ function ClinicalTestDataScreen() {
         var condition = 0;
         console.log(data.item.data_type)
 
+        // Data card condition colour switch statement
         switch (data.item.data_type) {
             case 'Blood Pressure':
                 var bloodPressure = data.item.reading_value.split("/")
@@ -111,71 +116,99 @@ function ClinicalTestDataScreen() {
                 bloodPressure[0] = parseInt(bloodPressure[0], 10)
                 bloodPressure[1] = parseInt(bloodPressure[1], 10)
 
-                if ((bloodPressure[0] >= 180) || (bloodPressure[1] >= 120)){
-                    condition = 5;
-                }
-                else if ((bloodPressure[0] >= 140) || (bloodPressure[1] >= 90)){
-                    condition = 4;
-                }
-                else if ((140 > bloodPressure[0] >= 130) && (90 > bloodPressure[1] >= 80)){
-                    condition = 3;
-                }
-                else if ((130 > bloodPressure[0] >= 120) && (bloodPressure[1] > 80)){
-                    condition = 2;
-                }
-                else {
-                    condition = 1;
-                }
+                if ((bloodPressure[0] >= 180) || (bloodPressure[1] >= 120)) condition = 5;
+                else if ((bloodPressure[0] >= 140) || (bloodPressure[1] >= 90)) condition = 4;
+                else if ((140 > bloodPressure[0] >= 130) && (90 > bloodPressure[1] >= 80)) condition = 3;
+                else if ((130 > bloodPressure[0] >= 120) && (bloodPressure[1] > 80)) condition = 2;
+                else condition = 1;
                 console.log(condition)
                 break;
             
             case 'Respiratory Rate':
+                var respiratoryRate = data.item.reading_value.split(" ")
+                respiratoryRate = parseInt(respiratoryRate[0])
 
+                if (respiratoryRate > 24) condition = 5;
+                else if (respiratoryRate > 20) condition = 3;
+                else if (20 >= respiratoryRate >= 12) condition = 1;
                 break;
             
             case 'Blood Oxygen Level':
+                var bloodOxygenLevel = data.item.data_value.split(" ")
+                bloodOxygenLevel = parseInt(bloodOxygenLevel[0])
 
+                if (95 <= bloodOxygenLevel) condition = 1;
+                else if (91 <= bloodOxygenLevel <= 95) condition = 2;
+                else if (85 < bloodOxygenLevel <= 90 ) condition = 3;
+                else if (80 < bloodOxygenLevel <= 85 ) condition = 4;
+                else if (bloodOxygenLevel <= 67) condition = 5;
                 break;
 
             case 'Heartbeat Rate':
+                var bmp = data.item.data_value.split(" ")
+                bmp = parseInt(bpm[0])
 
+                if (48 <= bmp <= 61) condition = 1;
+                else if (62 <= bmp <= 69) condition = 2;
+                else if (70 <= bmp <= 73) condition = 3;
+                else if (74 <= bmp <= 79) condition = 4;
+                else if (80 <= bmp) condition = 5;
                 break;
             
             default:
                 break;
         }
 
+        // Delete alert
+        const createAlert = () => 
+            Alert.alert("Delete Clinical Data", "Are you sure you want to delete this data entry?", [
+                {
+                    text: 'DELETE',
+                    onPress: () => {
+                        console.log("DELETE Pressed")
+                        deleteClinicalData(data.item._id)
+                        setRefreshIndicator(true)
+                        fetchTestDataDetails()
+                    }
+                },
+                {
+                    text: 'Cancel',
+                    onPress: () => console.log("Cancel Pressed")
+                }
+            ]);
+
+        // Data cards
         return (
             <MenuProvider skipInstanceCheck={true}>
-                    <View style={[ styles.card,
-                            condition==1?styles.cardBackground1:null ||
-                            condition==2?styles.cardBackground2:null ||
-                            condition==3?styles.cardBackground3:null ||
-                            condition==4?styles.cardBackground4:null ||
-                            condition==5?styles.cardBackground5:null 
-                        ]}>
-                        <View style={styles.cardContent}>
-                            <View style={{ flexDirection: 'row' }}>
-                                <Text style={[styles.typeText, condition==4?{color: '#fff'}:null || condition==5?{color: '#fff'}:null]}>{data.item.data_type}</Text>
-                                <Menu>
-                                    <MenuTrigger>
-                                        <Octicons name="three-bars" size={16} style={[{paddingTop: 10}, condition==4?{color: '#fff'}:null || condition==5?{color: '#fff'}:null]} />
-                                    </MenuTrigger>
-                                    <MenuOptions>
-                                        <MenuOption >
-                                            <Text>Edit</Text>
-                                        </MenuOption>
-                                        <MenuOption onSelect={() => alert(`Delete`)} >
-                                            <Text style={{color: 'red'}}>Delete</Text>
-                                        </MenuOption>
-                                    </MenuOptions>
-                                </Menu>
-                            </View>
-                            <Text style={[styles.readingText, condition==4?{color: '#fff'}:null || condition==5?{color: '#fff'}:null]}>{data.item.reading_value}</Text>
-                            <Text style={[styles.body2, condition==5?{color: '#fff'}:null || condition==4?{color: '#fff'}:null]}>{dateString} at {timeString}</Text>
+                <View style={[ styles.card,
+                        condition==1?styles.cardBackground1:null ||
+                        condition==2?styles.cardBackground2:null ||
+                        condition==3?styles.cardBackground3:null ||
+                        condition==4?styles.cardBackground4:null ||
+                        condition==5?styles.cardBackground5:null 
+                    ]}>
+                    <View style={styles.cardContent}>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Text style={[styles.typeText, condition==4?{color: '#fff'}:null || condition==5?{color: '#fff'}:null]}>{data.item.data_type}</Text>
+                            <Menu>
+                                <MenuTrigger>
+                                    <Octicons name="three-bars" size={16} style={[{paddingTop: 10}, condition==4?{color: '#fff'}:null || condition==5?{color: '#fff'}:null]} />
+                                </MenuTrigger>
+                                <MenuOptions>
+                                    <MenuOption >
+                                        <Text>Edit</Text>
+                                    </MenuOption>
+                                    <MenuOption onSelect={ () => createAlert() }>
+                                        <Text style={{color: 'red'}}>Delete</Text>
+                                    </MenuOption>
+                                </MenuOptions>
+                            </Menu>
                         </View>
-                        
+                        <Text style={[styles.readingText, condition==4?{color: '#fff'}:null || condition==5?{color: '#fff'}:null]}>{data.item.reading_value}</Text>
+                        <Text style={[styles.body2, condition==5?{color: '#fff'}:null || condition==4?{color: '#fff'}:null]}>{dateString} at {timeString}</Text>
                     </View>
+                    
+                </View>
             </MenuProvider>
         )
     }
@@ -186,9 +219,39 @@ function ClinicalTestDataScreen() {
                     data = {testDataDetails} 
                     renderItem = {item=> this.renderItem(item)}
                     ListEmptyComponent = {<Text>No Clinical Records</Text>}
+                    extraData={refreshIndicator}
                 />
             </View>
     )
+}
+
+function deleteClinicalData(id) {
+    console.log(id)
+
+    const deleteTestData = async () => {
+        await fetch('http://127.0.0.1:3000/patients/testdata/'+id, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                // Data deleted successfully
+                console.log('Data deleted');
+            } 
+            else {
+                // Error occurred while deleting data
+                console.log('Error deleting data');
+            }
+        })
+        .catch(error => {
+            // Error occurred while making the request
+            console.error('Error:', error);
+        });
+    };
+
+    return deleteTestData();
 }
 
 const styles = StyleSheet.create({
