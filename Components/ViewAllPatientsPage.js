385186205b6f4571ac0a270c48fd321e1bb08cae
Pui Yee Ng <hilarypyng@gmail.com>
2023-11-09 10:35:39 -0500

    Implemented delete patient feature

--- Components/ViewAllPatientsPage.js
+++ Components/ViewAllPatientsPage.js
@@ -1,34 +1,135 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, View, Image, FlatList } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, View, Image, FlatList, Alert } from 'react-native';
 import { Feather, Octicons } from "@expo/vector-icons";
 import Card from './card'
+import { MenuProvider } from 'react-native-popup-menu';
+import { Menu, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
 
 export default function PatientsList(props) {
   const [search, setSearch] = useState('');
   const [clicked, setClicked] = useState(false);
   const [patients, setPatients] = useState([]);
+  const [refreshIndicator, setRefreshIndicator] = useState(false);
+
+  const fetchPatients = async () => {
+    await fetch('http://127.0.0.1:3000/patients')
+      .then(response => response.json())
+      .then(data => setPatients(data))
+      .catch(error => console.log(error))
+  };
 
   useEffect(() => {
-    const fetchPatients = async () => {
-      await fetch('http://127.0.0.1:3000/patients')
-        .then(response => response.json())
-        .then(data => setPatients(data))
-        .catch(error => console.log(error))
-    };
     fetchPatients();
   }, []);
+
   renderItem = (data) => {
+
+    
+    function deletePatient(id) {
+      console.log(id)
+  
+    const deletePatientData = async () => {
+        await fetch('http://127.0.0.1:3000/patients/'+id, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                // Patient deleted successfully
+                console.log('Patient deleted');
+            } 
+            else {
+                // Error occurred while deleting data
+                console.log('Error deleting patient');
+            }
+        })
+        .catch(error => {
+            // Error occurred while making the request
+            console.error('Error:', error);
+        });
+    };
+  
+    return deletePatientData();
+  }
+  
+  function deletePatientClinicalData(id) {
+    console.log(id)
+  
+    const deleteRespectivePatientClinicalData = async () => {
+        await fetch('http://127.0.0.1:3000/patients/'+id + '/testdata', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                // Patient deleted successfully
+                console.log('Patient clinical data deleted');
+            } 
+            else {
+                // Error occurred while deleting data
+                console.log('Error deleting patient clinical data');
+            }
+        })
+        .catch(error => {
+            // Error occurred while making the request
+            console.error('Error:', error);
+        });
+    };
+  
+    return deleteRespectivePatientClinicalData();
+  }
+  
+    // Delete alert
+    const createAlert = () => 
+    Alert.alert("Delete Patient and Respective Clinical Data", "Are you sure you want to delete this data entry?", [
+        {
+            text: 'DELETE',
+            onPress: () => {
+                console.log("DELETE Pressed")
+                deletePatient(data.item._id)
+                deletePatientClinicalData(data.item._id)
+                setRefreshIndicator(true)
+                fetchPatients()
+            }
+        },
+        {
+            text: 'Cancel',
+            onPress: () => console.log("Cancel Pressed")
+        }
+    ]);
+
     return (
       <TouchableOpacity onPress={() => props.navigation.navigate('PatientDetails', {
         id: data.item._id
       })}>
+        <MenuProvider skipInstanceCheck={true}>
         <Card>
-          <Text style={styles.text}>{data.item.first_name} {data.item.last_name}</Text>
-          <TouchableOpacity onPress={()=>{}}>
-            <Octicons name="three-bars" size={16} style={{paddingTop: 5}} />
-          </TouchableOpacity>
+          <Text style={styles.text}>{data.item.first_name} {data.item.last_name}</Text>     
+          <Menu>
+            <MenuTrigger>
+              <Octicons name="three-bars" size={20} />
+            </MenuTrigger>
+            <MenuOptions>
+                <MenuOption >
+                    <Text>Edit</Text>
+                </MenuOption>
+                <MenuOption onSelect={ () => 
+                { setRefreshIndicator(false)
+                  createAlert() 
+                }
+                }>
+                    <Text style={{color: 'red'}}>Delete</Text>
+                </MenuOption>
+            </MenuOptions>
+        </Menu>
         </Card>
+        </MenuProvider>
+        
       </TouchableOpacity>
     )
   }
@@ -64,6 +165,7 @@ export default function PatientsList(props) {
       <FlatList
         data = {patients} 
         renderItem = {item=> this.renderItem(item)}
+        extraData={refreshIndicator}
       />     
     </View>
   )
